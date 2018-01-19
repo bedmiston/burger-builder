bb15265bf61e9758ceede57115504c9890f947f5
Brendon Edmiston <Brendon_Edmiston@reyrey.com>
2018-01-19 14:32:34 -0600

    Add framework for dynamically adding BurgerIngredients to the Burger.

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -2,14 +2,39 @@ import * as React from 'react';
 import * as styles from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-interface BurgerProps {}
+interface IngredientProps {
+  salad?: number;
+  cheese?: number;
+  meat?: number;
+  bacon: number;
+}
 
-const Burger: React.SFC<BurgerProps> = props => {
+interface BurgerProps {
+  ingredients: IngredientProps;
+}
+
+const Burger: React.SFC<BurgerProps> = (props) => {
+  let transformedIngredients: JSX.Element[] | null = Object.keys(
+    props.ingredients
+  )
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey]).fill(<React.Fragment />)].map(
+        (_, i) => {
+          return <BurgerIngredient key={igKey + i} type={igKey} />;
+        }
+      );
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []); // tslint:disable-line
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = [<p key="0">Please start adding ingredients!</p>];
+  }
+
   return (
     <div className={styles.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.tsx
+++ src/containers/BurgerBuilder/BurgerBuilder.tsx
@@ -3,10 +3,19 @@ import Burger from '../../components/Burger/Burger';
 export interface BurgerBuilderProps {}
 
 class BurgerBuilder extends React.Component<BurgerBuilderProps> {
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    }
+  };
+
   render() {
     return (
       <React.Fragment>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </React.Fragment>
     );
