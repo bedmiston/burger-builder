8c62ef6ed07fa851ba945ac9bcfde7761e126afe
Brendon Edmiston <Brendon_Edmiston@reyrey.com>
2018-02-05 14:48:17 -0600

    Use styled-components for SideDrawer component.

--- src/components/Navigation/SideDrawer/SideDrawer.css.d.ts
+++
@@ -1,4 +0,0 @@
-export const SideDrawer: string;
-export const Open: string;
-export const Close: string;
-export const Logo: string;
--- src/components/Navigation/SideDrawer/SideDrawer.tsx
+++ src/components/Navigation/SideDrawer/SideDrawer.tsx
@@ -3,30 +3,52 @@ import * as React from 'react';
 import Logo from '../../Logo/Logo';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 import NavigationItems from '../NavigationItems/NavigationItems';
-import * as styles from './SideDrawer.css';
+// import * as styles from './SideDrawer.css';
+import styled from 'styled-components';
+
+const StyledDiv = styled('div')`
+  position: fixed;
+  width: 280px;
+  max-width: 70%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  z-index: 200;
+  background-color: white;
+  padding: 32px 16px;
+  box-sizing: border-box;
+  transition: transform 0.3s ease-out;
+  transform: ${(props: { open: boolean }) =>
+    props.open ? 'translateX(0)' : 'translateX(-100%)'};
+
+  @media (min-width: 500px) {
+    display: none;
+  }
+`;
+
+const LogoDiv = styled('div')`
+  height: 11%;
+  margin-bottom: 32px;
+`;
 
 interface SideDrawerProps {
   open: boolean;
   closed: () => void;
 }
 
-const SideDrawer: React.SFC<SideDrawerProps> = (props) => {
-  let attachedClasses = [styles.SideDrawer, styles.Close];
-  if (props.open) {
-    attachedClasses = [styles.SideDrawer, styles.Open];
-  }
+const SideDrawer: React.SFC<SideDrawerProps> = props => {
   return (
     <React.Fragment>
       <Backdrop show={props.open} clicked={props.closed} />
-      <div className={attachedClasses.join(' ')}>
-        <div className={styles.Logo}>
+      <StyledDiv {...props}>
+        <LogoDiv>
           <Logo />
-        </div>
+        </LogoDiv>
 
         <nav>
           <NavigationItems />
         </nav>
-      </div>
+      </StyledDiv>
     </React.Fragment>
   );
 };
