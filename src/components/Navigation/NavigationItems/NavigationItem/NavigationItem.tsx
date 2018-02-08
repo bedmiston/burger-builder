7c41717f141f749c1b385d255eeabe90f057e036
Brendon Edmiston <Brendon_Edmiston@reyrey.com>
2018-02-08 14:45:48 -0600

    Attempt to convert NavigationItem to styled-components.

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.tsx
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.tsx
@@ -1,17 +1,60 @@
 import * as React from 'react';
-import * as styles from './NavigationItem.css';
+import styled from 'styled-components';
+
+const StyleListItem = styled('li')`
+  margin: 10px 0;
+  box-sizing: border-box;
+  display: block;
+  width: 100%;
+
+  @media (min-width: 500px) {
+    margin: 0;
+    display: flex;
+    height: 100%;
+    width: auto;
+    align-items: center;
+  }
+`;
+
+interface StyledLinkProps {
+  active: boolean;
+}
+
+const StyledLink = styled<StyledLinkProps, 'a'>('a')`
+  color: ${props => (props.active ? '#40a4c8' : '#8f5c2c')};
+  text-decoration: none;
+  height: 100%;
+  box-sizing: border-box;
+  display: block;
+
+  &:hover {
+    color: #40a4c8;
+  }
+
+  @media (min-width: 500px) {
+    color: white;
+    height: 100%;
+    padding: 16px 10px;
+    border-bottom: 4px solid transparent;
+
+    &:hover {
+      background-color: #8f5c2c;
+      border-bottom: 4px solid #40a4c8;
+    }
+  }
+`;
 
 interface NavigationItemProps {
   link: string;
   active: boolean;
 }
 
-const NavigationItem: React.SFC<NavigationItemProps> = (props) => (
-  <li className={styles.NavigationItem}>
-    <a href={props.link} className={props.active ? styles.active : undefined}>
+const NavigationItem: React.SFC<NavigationItemProps> = props => (
+  <StyleListItem>
+    <StyledLink href={props.link} active={props.active}>
       {props.children}
-    </a>
-  </li>
+    </StyledLink>
+  </StyleListItem>
 );
 
 export default NavigationItem;
